c7d93b3def6fcaa43f9e650e254f128be32959b6
sifisega <hallis1002@gmail.com>
2020-02-11 09:04:45 +1300

    Fixed some errors, added button name and new button id structure

--- home.js
+++ home.js
@@ -1,10 +1,12 @@
 let menu = {
-  flatwhite:{price:4,containsMilk:true,},
-  cappachino:{price:5,containsMilk:true},
-  latte:{price:5,containsMilk:true},
+  flatwhite:{price:3,containsMilk:true,},
+  cappachino:{price:3,containsMilk:true},
+  latte:{price:3,containsMilk:true},
   longblack:{price:3,containsMilk:false},
   shortblack:{price:3,containsMilk:false},
-  machiato:{price:4,containsMilk:true}}; // the full list of menu items (everything that can be ordered)
+  hotchocolate:{price:2,containsMilk:true},
+  machiato:{price:2,containsMilk:true},
+  picollo:{price:2,containsMilk:true}} ; // the full list of menu items (everything that can be ordered)
 let buttonLocations = [document.getElementById("buttonContainer"),document.getElementById('itemsOutput'),];
 let menuKeys = Object.keys(menu);
 let currentButtons = [];
@@ -16,7 +18,6 @@ let items = [];
 
 //Creates the initial buttons using 'menu'
   for(i=0;i<menuKeys.length;i++){
-    console.log(menuKeys[i]);
     addButton(menuKeys[i],menuKeys[i],0,0);
   }
 
@@ -29,7 +30,6 @@ function doSearch() {   // Will refine displayed buttons based on a .test() call
       searchInput.push(menuKeys[i]);
     }
   }
-  console.log('searching for --' + search + '--. Results are --' + searchInput + '--.');
   updateButtons();
 }
 
@@ -39,39 +39,32 @@ function updateButtons() {
   for (var i = 0; i < currentButtons.length; i++) {
     if (!searchInput.includes(currentButtons[i])){
       let tempButton = document.getElementById(currentButtons[i]);
-      console.log('removing currentButton --' + currentButtons[i] + '--. removing button node --' + tempButton.id + '--');
       tempButton.parentNode.removeChild(tempButton);
       currentButtons.splice(i,1);
       i=i-1;
     }
   }// Find buttons to remove
-  console.log('before splicing, the searchInput is --' + searchInput +'--');
   for(i = 0 ; i < searchInput.length; i++) {
-    console.log('currentButtons = --' + currentButtons + '-- searchInput[' + i + '] = --' + searchInput[i] + '--');
       if (currentButtons.includes(searchInput[i])){
-        console.log('Splicing --' + searchInput[i] + '--');
         searchInput.splice(i,1);
-        console.log('remaining searchInput is --' + searchInput + '--');
         i=i-1;
       }
   }// Find buttons to keep and removes them from searchInput
   for (var x = 0; x < searchInput.length; x++) {
-    console.log("searchInput"+searchInput);
-    addButton(searchInput[x],0,0);
+    addButton(searchInput[x],searchInput[x],0,0);
   }//adds buttons
 }
 
 
-function buttonPress(buttonId,name,action){ // Will process the button id and 'action'(0 for addition, 1 for subtraction) to either add or subtract an order
+// Will process the button id and 'action'(0 for addition, 1 for subtraction) to either add or subtract an order
+function buttonPress(buttonId,name,action) {
   if (action == 0){
-    console.log(name);
     subTotal = subTotal + menu[name]['price'];
     /*let ul = document.getElementById('itemsOutput');
     let li = document.createElement('li');
     li.innerHTML = (menu[buttonId] + ' ' + menu[buttonId]['price'])
     li.onclick = function */
     addButton(buttonId,name,1,1);
-    console.log(buttonId);
   } else if (action == 1){
     subTotal = subTotal - menu[name]['price'];
     removeButton(buttonId,1);
@@ -83,6 +76,7 @@ function buttonPress(buttonId,name,action){ // Will process the button id and 'a
 
 function addButton(buttonId, name, buttonLocation, pressAction){
     let button = document.createElement("button");
+    console.log('8' + name);
     button.innerHTML = name + ' $' + menu[name]['price'];
     button.id = buttonId + buttonLocation; // defines button id eg. id='flatWhite1'
     button.name = name;
@@ -94,8 +88,6 @@ function addButton(buttonId, name, buttonLocation, pressAction){
 function removeButton(buttonId, buttonLocation) {
   let garbage = buttonLocations[buttonLocation];
   let child = document.getElementById(buttonId);
-  console.log(child);
-  console.log(garbage);
   garbage.removeChild(child);
 }
 
